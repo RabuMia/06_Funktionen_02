071598d962d124d1930ab63253a5e07662331a94
RabuMia <Excelchen@hotmail.de>
2021-08-02 11:19:34 +0200

    3 & 4 ++

--- script_01.js
+++ script_01.js
@@ -5,6 +5,21 @@
 1. Dateneingabe + -überprüfung 
 2. Auswahl Rechenart 
 3. Fkt. Grundrechenarten 
-4. Ausgabe in Konsole 
+4. Ausgabe in Konsole : check!
 */
 
+
+// Modul: Addition a + b | Test:
+ausgabe(addieren(2,1));
+function addieren(a,b) {
+    //const c = a + b;
+    //return c; zu lang und komplex
+    return a + b;
+}
+
+// Modul: Konsolenausgabe | Test:
+//ausgabe("Hallo Welt!");
+//ausgabe(20);
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
